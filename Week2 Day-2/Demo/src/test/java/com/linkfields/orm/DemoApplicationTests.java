ef787bae14cb42f7083bb27ffe0c143589820141
VineethGoudGit <cineethgoudvgs789@gmail.com>
2025-04-28 11:34:31 +0530

    commited

---
+++ Week2 Day-2/Demo/src/test/java/com/linkfields/orm/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkfields.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
